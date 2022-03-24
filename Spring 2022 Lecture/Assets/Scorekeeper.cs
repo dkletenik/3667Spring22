72702405349ef7e7ebe1007fd9d139836732e8f9
dkletenik <kletenik@sci.brooklyn.cuny.edu>
2022-03-24 11:40:03 -0400

    3/24 Unity Lecture 4: text and scenes

--- Spring 2022 Lecture/Assets/Scorekeeper.cs
+++ Spring 2022 Lecture/Assets/Scorekeeper.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Scorekeeper : MonoBehaviour
 {
     [SerializeField] int score = 0;
     const int DEFAULT_POINTS = 1;
+    [SerializeField] Text scoreTxt;
+    [SerializeField] Text levelTxt;
+    [SerializeField] int level;
+    const int SCORE_THRESHOLD = 10;
 
     // Start is called before the first frame update
     void Start()
     {
+        level = SceneManager.GetActiveScene().buildIndex;
 
+        //display score
+        DisplayScore();
+        DisplayLevel();
     }
 
     // Update is called once per frame
@@ -23,10 +33,29 @@ public class Scorekeeper : MonoBehaviour
     {
         score += addend;
         Debug.Log("score" + score);
+        //display score
+        DisplayScore();
+
+        if (score > SCORE_THRESHOLD)
+        {
+            //move on to next level
+            SceneManager.LoadScene(level + 1);
+        }
     }
 
     public void UpdateScore()
     {
         UpdateScore(DEFAULT_POINTS);
+    }
+
+    public void DisplayScore()
+    {
+        scoreTxt.text = "Score: " + score;
+    }
+
+    public void DisplayLevel()
+    {
+        int levelToDisplay = level + 1;
+        levelTxt.text = "Level " + levelToDisplay;
     }
 }
